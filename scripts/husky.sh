56aa53e921f854748acf04387c0c4e3aa0d5933a
Higor Alves <higorhaalves@gmail.com>
2023-06-14 14:46:51 -0300

    chore: adding github actions

--- scripts/husky.sh
+++ scripts/husky.sh
@@ -7,6 +7,6 @@ rm -fr .husky
 yarn husky install
 yarn husky add .husky/pre-push "yarn lint"
 yarn husky add .husky/pre-commit "yarn audit:commit"
-yarn husky add .husky/commit-msg "git-conventional-commits commit-msg-hook $commit_message"
+yarn husky add .husky/commit-msg "npx commitlint --edit $1"
 
 echo "FILES ARE NOW PRETTY AGAIN"

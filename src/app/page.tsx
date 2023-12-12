1c8995b90af478bab9d17664cee829e0151d6e2e
Higor Alves <me@higoralves.dev>
2023-12-12 18:19:11 -0300

    chore: updating

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,8 @@
 'use client'
-import { useEffect } from 'react'
 import type { NextPage } from 'next'
 import { Title } from '@mantine/core'
-import contentfulClient from '~/services/contentful/client'
 
 const Home: NextPage = () => {
-  async function test() {
-    const pages = await contentfulClient.getEntries<any>({
-      include: 4,
-      content_type: 'article',
-    })
-
-    console.log(pages)
-  }
-
-  useEffect(() => {
-    test()
-  })
   return (
     <>
       <Title>Next Enterprise Boilerplate</Title>

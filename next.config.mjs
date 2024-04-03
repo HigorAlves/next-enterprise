38a90ec856e69fd7cf609d41a93e3c455956a03d
Higor Alves <me@higoralves.dev>
2024-04-02 21:56:17 -0300

    chore(api) Update configuration and add health check API
    Updated Next.js' configuration file with settings such as `reactStrictMode`, `trailingSlash`, and `productionBrowserSourceMaps`. Installed `@t3-oss/env-nextjs` and `zod` as dependencies and new health check API route added. This update also includes creation of `env.mjs` file to handle environment variables. Lastly, added new rules for commit message inspections to aid in maintaining a clean commit history.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	reactStrictMode: true,
+	trailingSlash: true,
+	productionBrowserSourceMaps: true,
+	rewrites() {
+		return [
+			{source: '/healthz', destination: '/api/health'},
+			{source: '/api/healthz', destination: '/api/health'},
+			{source: '/health', destination: '/api/health'},
+			{source: '/ping', destination: '/api/health'},
+		]
+	},
+};
 
 export default nextConfig;
